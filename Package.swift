e9125e45ea3ef478ba72837c45e81ee7e046faf4
Michael Dales <michael@digitalflapjack.com>
2022-08-20 15:35:07 +0100

    Remove unused dependancy.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "GuiltySpark",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.1"),
         .package(url: "https://github.com/NozeIO/MicroExpress.git", from: "0.5.3"),

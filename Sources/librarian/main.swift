c61df4c46c0ef3b82b9d84dee6d2c2792b293e98
Michael Dales <michael@digitalflapjack.com>
2022-09-01 10:51:58 +0100

    Refactor server top level to remove monolithic run function.

--- Sources/librarian/main.swift
+++ Sources/librarian/main.swift
@@ -10,69 +10,82 @@ struct Config: Codable {
 	let bibliographyFilePath: String
 }
 
+final class Server {
+
+	let corpusConfigPath: String
+	let library: Library
+	let server: Express
+	let port = 4242
+
+	init(_ corpusConfigPath: String) throws {
+		self.corpusConfigPath = corpusConfigPath
+		self.library = try Library.loadConfig(configPath: corpusConfigPath)
+
+		server = Express()
+		server.use { req, res, next in
+			 print("\(req.header.method):", req.header.uri)
+			next()
+		}
+		server.use(querystring)
+		server.get("/search", middleware: searchHandler)
+	}
+
+	func run() {
+		server.listen(port)
+	}
+
+	func searchHandler (req: IncomingMessage, res: ServerResponse, next: Next) -> Void {
+		var corpusSelection: String? = nil
+
+		// try to find which corpus to use by header
+		if let header = req.header.headers.filter({ $0.0 == "X-Corpus"}).first {
+			corpusSelection = header.1
+		}
+		// query arg takes precedence over header
+		if let corpus_arg = req.param("corpus") {
+			corpusSelection = corpus_arg;
+		}
+		guard let corpusSelection = corpusSelection else {
+			res.send("No corpus secified")
+			return
+		}
+
+		guard let query = req.param("q") else {
+			res.send("No info")
+			return
+		}
+		guard let decoded_query = query.removingPercentEncoding else {
+			res.send("Failed to decode")
+			return
+		}
+
+		let results: [BibPage]
+		do {
+			results = try library.find(corpus: corpusSelection, query: decoded_query)
+		} catch LibraryError.CorpusNotFound {
+			res.send("Corpus \(corpusSelection) not found")
+			return
+		} catch {
+			res.send("Unexpected error: \(error)")
+			return
+		}
+
+		res.json(results)
+	}
+}
+
 struct Searcher: ParsableCommand {
 	@Argument() var corpusConfigPath: String
 
 	func run() {
-		let library: Library
+		let server: Server
 		do {
-			library = try Library.loadConfig(configPath: corpusConfigPath)
+			server = try Server(corpusConfigPath)
 		} catch {
-			print("Failed to load corpus: \(error)")
+			print("Failed to construct server: \(error)")
 			return
 		}
-
-		let server = Express()
-		server.use { req, res, next in
-		 	print("\(req.header.method):", req.header.uri)
-		    next()
-		}
-		server.use(querystring)
-		server.get("/search/") {req, res, next in
-
-			var corpusSelection: String? = nil
-
-			// try to find which corpus to use by header
-			if let header = req.header.headers.filter({ $0.0 == "X-Corpus"}).first {
-				corpusSelection = header.1
-			}
-			// query arg takes precedence over header
-			if let corpus_arg = req.param("corpus") {
-				corpusSelection = corpus_arg;
-			}
-			guard let corpusSelection = corpusSelection else {
-				res.send("No corpus secified")
-				return
-			}
-
-			guard let query = req.param("q") else {
-				res.send("No info")
-				return
-			}
-			guard let decoded_query = query.removingPercentEncoding else {
-				res.send("Failed to decode")
-				return
-			}
-
-			let results: [BibPage]
-			do {
-				results = try library.find(corpus: corpusSelection, query: decoded_query)
-			} catch LibraryError.CorpusNotFound {
-				res.send("Corpus \(corpusSelection) not found")
-				return
-			} catch {
-				res.send("Unexpected error: \(error)")
-				return
-			}
-
-			res.json(results)
-		}
-
-		server.get("/") { req, res, next in
-		  	res.send("Hello, World!")
-		}
-
-		server.listen(4242)
+		server.run()
 	}
 }
 
